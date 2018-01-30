ff29fc401d87a8e6659edaba99ece4730a5dfff9
Pawel Zun <pzun90@gmail.com>
2018-01-30 23:09:25 +0100

    finish app

--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -5,18 +5,14 @@ import DevTools from '../DevTools';
 
 const Navigation = props => (
   <div>
-    <nav className="navbar navbar-light bg-light">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <Link className="navbar-brand" to="/">Countries.js</Link>
-        </div>
-        <div className="navbar-collapse">
-          <ul className="nav navbar-nav">
-            <li><Link to="/countries">Countries</Link></li>
-            <li><Link to="/continents">Continents</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </div>
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <Link className="navbar-brand h1" to="/">Countries.js</Link>
+      <div className="navbar">
+        <ul className="navbar-nav">
+          <li><Link to="/countries" className="nav-link">Countries</Link></li>
+          <li><Link to="/continents" className="nav-link">Continents</Link></li>
+          <li><Link to="/contact" className="nav-link">Contact</Link></li>
+        </ul>
       </div>
     </nav>
     <div className="container-fluid">
